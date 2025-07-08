a0483f49367e78cfef7de9342dbab35a084f6170
monaim3 <monaimmukul75@gmail.com>
2025-07-08 16:38:12 +0600

    rtk query update

--- src/store/services/api.js
+++ src/store/services/api.js
@@ -1,14 +1,27 @@
 // services/api.js
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://jsonplaceholder.typicode.com/' }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://apex.oracle.com/pls/apex/beesoft/auth/',
+  }),
   endpoints: (builder) => ({
     getPosts: builder.query({
-      query: () => 'posts',
+      query: () => 'https://jsonplaceholder.typicode.com/posts',
+    }),
+
+    login: builder.mutation({
+      query: ({ roll, password }) => ({
+        url: 'login',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: { roll, password },
+      }),
     }),
   }),
-})
+});
 
-export const { useGetPostsQuery } = api
+export const { useGetPostsQuery, useLoginMutation } = api;
